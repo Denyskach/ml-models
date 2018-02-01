64cf239e9356e39e61517d0c6f6499fddc65e103
denys <denuskach@gmail.com>
2018-02-01 01:28:26 -0500

    Dog breed: Fix issue with prediction flow

--- kaggle/dog-breed-identification/utils.py
+++ kaggle/dog-breed-identification/utils.py
@@ -1,30 +1,6 @@
-from glob import glob
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import KFold
-
-from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16
-from keras.applications.inception_v3 import InceptionV3
-from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
-from keras.layers import Dense
-from keras.layers import BatchNormalization
-from keras.layers import Flatten
-from keras.layers import Dropout
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.models import Sequential
-from keras.models import Model
-from keras.optimizers import Adam
-
-from keras.regularizers import l2
 
 def plot_learning_curve(path):
     training_df = pd.read_csv(path)
