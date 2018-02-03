9d890084a6eb74ca9216e937a1e57a78aeb9f3fe
denys <denuskach@gmail.com>
2018-02-03 17:35:53 -0500

    dog breed: new models

--- kaggle/dog-breed-identification/utils.py
+++ kaggle/dog-breed-identification/utils.py
@@ -1,6 +1,14 @@
+import bcolz
+
+from keras.preprocessing import image
+
+import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import OneHotEncoder
 
 def plot_learning_curve(path):
     training_df = pd.read_csv(path)
@@ -22,3 +30,20 @@ def plot_learning_curve(path):
 
 
     plt.show()
+    
+def save_array(fname, arr): c=bcolz.carray(arr, rootdir=fname, mode='w'); c.flush()
+def load_array(fname): return bcolz.open(fname)[:]
+
+def get_batches(dirname, gen=image.ImageDataGenerator(), shuffle=True, batch_size=4, class_mode='categorical',
+                target_size=(244, 244)):
+    return gen.flow_from_directory(dirname, target_size=target_size, 
+                                   class_mode=class_mode, shuffle=shuffle, batch_size=batch_size)
+
+def get_data(path, target_size=(244, 244)):
+    batches = get_batches(path, shuffle=False, batch_size=1, class_mode=None, target_size=target_size)
+    return np.concatenate([batches.next() for i in range(batches.samples)])
+
+def get_breed(labels_df, id):
+    return labels_df.loc[labels_df["id"] == id]["breed"].values[0]
+
+def onehot(x): return np.array(OneHotEncoder().fit_transform(x.reshape(-1,1)).todense())
